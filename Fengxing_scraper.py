6bdc5e4bea4d911a9ded5e161b7cc50fbe1dc39b
Shawn Li <shawnli2010@126.com>
2016-04-14 22:16:59 -0700

    Working on Fengxing_scraper each movie page timeout setting

--- Fengxing_scraper.py
+++ Fengxing_scraper.py
@@ -13,9 +13,11 @@ db = MySQLdb.connect("localhost","root","0924xiaopan","JHSDB" )
 db_cursor = db.cursor()
 db_cursor.execute("SET NAMES UTF8")
 
-url = "http://www.fun.tv/retrieve/c-e794b5e5bdb1.n-e5bdb1e78987.o-sc.pg-1.uc-23"
+movie_list_url = "http://www.fun.tv/retrieve/c-e794b5e5bdb1.n-e5bdb1e78987.o-sc.pg-1.uc-23"
 chromedriver = webdriver.Chrome()
-chromedriver.get(url)
+chromedriver.get(movie_list_url)
+
+root_url = "http://www.fun.tv"
 
 html = chromedriver.page_source
 bs = BeautifulSoup(html)
@@ -25,12 +27,31 @@ first_movie_box = movie_list_div.find("div",{"class":"mod-vd-i first"})
 movie_boxes = movie_list_div.findAll("div",{"class":"mod-vd-i  "})
 movie_boxes.append(first_movie_box)
 
-count = 0
-for movie_box in movie_boxes:
-    print movie_box
-    count = count + 1
+for each_div in movie_boxes:
+    info_div = each_div.find("div",{"class":"info"})
+    movie_name = info_div.find("a").text
+    movie_score = info_div.find("b").text
 
-print count    
+    sub_link_string = info_div.find("a")['href']
+    movie_url = root_url + sub_link_string
+    chromedriver.set_page_load_timeout(5)
+    try:
+        chromedriver.get(movie_url)
+    except Exception, exception:
+        if isinstance(exception, TimeoutError):
+            print "aaaaaaaaaa"
+        else:
+            print "99999"
+    # '''Click into each movie to get the views of each movie'''
+    # movie_link = chromedriver.find_element_by_link_text(movie_name)
+    # movie_link.click()
+
+    # time.sleep(10)
+
+    # print chromedriver.page_source
+    
+    break
+    
 
 
 
